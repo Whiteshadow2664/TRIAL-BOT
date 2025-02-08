c07e650c33273f5c24ac94e92b78ee569f4bf59d
Ds777774 <sjemma819@gmail.com>
2025-02-08 18:10:29 +0530

    Update modrank.js

--- modrank.js
+++ modrank.js
@@ -154,6 +154,7 @@ async function executeModRank(message) {
       }
 
       let leaderboard = '** **\n\n';
+
 result.rows.forEach((row, index) => {
   const avgPoints = (row.points / row.days_as_mod).toFixed(2);
   leaderboard += `**#${index + 1}** | **${row.days_as_mod} Days** | **${row.username}** - **P:** ${row.points} | **AVG:** ${avgPoints}\n`;
@@ -194,9 +195,8 @@ async function executeBumpLeaderboard(message) {
         return message.channel.send('No bumps recorded yet.');
       }
 
-      let leaderboard = '** **\n\n'; // Add two newline characters for space
+      let leaderboard = '** **\n\n';
 
-// Loop through and format the leaderboard entries
 result.rows.forEach((row, index) => {
   leaderboard += `**#${index + 1}** | **${row.username}** - **${row.bumps} Bumps**\n`;
 });
