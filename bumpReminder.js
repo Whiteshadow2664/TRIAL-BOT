05268c136bbeac638aa5347e7f2127e8b3954b6c
Ds777774 <sjemma819@gmail.com>
2025-02-08 15:11:55 +0530

    Update bumpReminder.js

--- bumpReminder.js
+++ bumpReminder.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
 
+// Disboard bot ID and settings
 const BUMP_BOT_ID = '302050872383242240'; // Disboard bot ID
 const REMINDER_ROLE_NAME = 'bump me'; // Role to be pinged for reminders
 const REMINDER_CHANNEL_ID = '1337072576597463084'; // Target channel for reminders
@@ -7,6 +8,7 @@ const REMINDER_INTERVAL = 2 * 60 * 60 * 1000; // 2 hours in milliseconds
 
 module.exports = {
     startBumpReminder(client) {
+        // Reminder every 2 hours to bump
         setInterval(async () => {
             const guild = client.guilds.cache.first(); // Get the first available guild
             if (!guild) return;
@@ -31,18 +33,17 @@ module.exports = {
 
         if (message.embeds.length > 0) {
             const embed = message.embeds[0];
+
+            // Check for the "Bump done!" message
             if (embed.description && embed.description.includes('Bump done! :thumbsup:')) {
-                // Extract the user who bumped from the embed
+                // Extract the user who performed the bump
                 const userMatch = embed.description.match(/<@!?(\d+)>/);
-                if (!userMatch) return;
+                if (userMatch) {
+                    const userId = userMatch[1];
 
-                const bumperId = userMatch[1];
-
-                // Send the after bump message thanking the user and schedule the next reminder
-                message.channel.send(`Thx for bumping our Server! We will remind you in 2 hours! <@${bumperId}>`);
-
-                // Optionally you can log this in your console as well for testing
-                console.log(`User ${bumperId} bumped the server.`);
+                    // Send a thank-you message to the user who bumped
+                    message.channel.send(`Thx for bumping our Server! We will remind you in 2 hours! <@${userId}>`);
+                }
             }
         }
     }
