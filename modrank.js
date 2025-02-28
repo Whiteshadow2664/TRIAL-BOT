41888fd17d21a4c1b23b4edaefef79f1d4c7e1a4
Whiteshadow2664 <sjemma819@gmail.com>
2025-02-28 12:38:02 +0530

    Update modrank.js

--- modrank.js
+++ modrank.js
@@ -50,7 +50,7 @@ module.exports.updateModRank = async (userId, username, guild) => {
 };
 
 // ✅ Function to save cached XP to DB at 15:15 IST daily
-cron.schedule('53 20 * * *', async () => {
+cron.schedule('43 12 * * *', async () => {
     console.log('⏳ Writing XP data to database...');
 
     if (xpCache.size === 0) {
