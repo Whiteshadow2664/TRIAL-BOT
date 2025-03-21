8870cc08fe55c93095e21f9348d2946191b1aaf0
Whiteshadow2664 <sjemma819@gmail.com>
2025-03-21 17:16:47 +0530

    Update event.js

--- event.js
+++ event.js
@@ -1,53 +1,23 @@
-const { EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     name: 'event',
     description: 'Create an event by filling out a form.',
     async execute(message) {
-        // Check if the command was used in a guild
+        // Check if the command is used in a guild
         if (!message.guild) {
             return message.reply('This command can only be used in a server.');
         }
 
-        // Create a modal (pop-up form)
-        const modal = new ModalBuilder()
-            .setCustomId('eventForm')
-            .setTitle('Create an Event');
+        // Create a button to trigger the modal
+        const eventButton = new ButtonBuilder()
+            .setCustomId('openEventModal')
+            .setLabel('Create Event')
+            .setStyle(ButtonStyle.Primary);
 
-        // Event Name Input
-        const eventNameInput = new TextInputBuilder()
-            .setCustomId('eventName')
-            .setLabel('Event Name')
-            .setStyle(TextInputStyle.Short)
-            .setRequired(true);
+        const row = new ActionRowBuilder().addComponents(eventButton);
 
-        // Event Date Input
-        const eventDateInput = new TextInputBuilder()
-            .setCustomId('eventDate')
-            .setLabel('Event Date & Time (e.g., 25th March, 5 PM IST)')
-            .setStyle(TextInputStyle.Short)
-            .setRequired(true);
-
-        // Event Description Input
-        const eventDescInput = new TextInputBuilder()
-            .setCustomId('eventDesc')
-            .setLabel('Event Description')
-            .setStyle(TextInputStyle.Paragraph)
-            .setRequired(true);
-
-        // Create Action Rows
-        const firstRow = new ActionRowBuilder().addComponents(eventNameInput);
-        const secondRow = new ActionRowBuilder().addComponents(eventDateInput);
-        const thirdRow = new ActionRowBuilder().addComponents(eventDescInput);
-
-        // Add components to the modal
-        modal.addComponents(firstRow, secondRow, thirdRow);
-
-        try {
-            await message.author.send({ content: 'Please fill out the event form:', components: [modal] });
-        } catch (error) {
-            console.error('Error sending modal:', error);
-            return message.reply('I couldn’t send you the event form. Please check your DMs.');
-        }
-    },
+        // Send the message with the button
+        await message.reply({ content: 'Click the button below to create an event.', components: [row] });
+    }
 };
