8e40c75dd8d90f4d3c567932efbd39c0e9241821
Whiteshadow2664 <sjemma819@gmail.com>
2025-03-27 10:42:28 +0530

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -2,10 +2,10 @@ const { PermissionFlagsBits } = require("discord.js");
 
 module.exports = {
     name: "mute",
-    description: "Mutes a mentioned user.",
+    description: "Mutes a mentioned user (Moderator only).",
     execute: async (message) => {
-        if (!message.member.permissions.has(PermissionFlagsBits.ModerateMembers)) {
-            return message.reply("❌ You don't have permission to use this command.");
+        if (!message.member.roles.cache.some(role => role.name === "Moderator")) {
+            return message.reply("❌ You must have the **Moderator** role to use this command.");
         }
 
         const user = message.mentions.users.first();
